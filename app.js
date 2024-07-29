7cb2a5964f99fedf1a0afb0e5426362e836db5f8
Siddhanth <123siddhanth@gmail.com>
2024-07-30 00:54:16 +0530

    added port in app.js

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
+const PORT = process.env.PORT || 8080;
 
 const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
