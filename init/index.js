831a9bf35754d8024dfdac9a60ac13652769bed1
siddhanth4 <123siddhanth@gmail.com>
2026-07-05 23:11:06 +0530

    Updated seeder code

--- init/index.js
+++ init/index.js
@@ -60,3 +60,85 @@ const initDB = async () => {
 };
 
 initDB();
+
+
+
+
+
+// const mongoose = require("mongoose");
+// const initData = require("./data");
+// const Listing = require('../models/listing.js');
+// // const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+// require("dotenv").config();
+// mongoose.connect(process.env.ATLASDB_URL);
+// main()
+//    .then(() => {
+//       console.log("connected to db");
+//    })  
+//    .catch((err) => {
+//       console.log(err);
+//    });
+
+// async function main(){
+//     await mongoose.connect(MONGO_URL);
+// }
+
+// const initDB = async () =>{
+//     await Listing.deleteMany({});
+//     initData.data = initData.data.map((obj) => ({...obj, owner: "66a1c580efb2abfd8ed102e6"}));
+//     await Listing.insertMany(initData.data);
+//     console.log("data was initialized");
+// }
+// initDB();
+
+
+// require("dotenv").config();
+
+// const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+
+// const geocodingClient = mbxGeocoding({
+//     accessToken: process.env.MAP_TOKEN,
+// });
+
+// const mongoose = require("mongoose");
+// const initData = require("./data");
+// const Listing = require("../models/listing");
+
+// async function main() {
+//   try {
+//     await mongoose.connect(process.env.ATLASDB_URL);
+//     console.log("Connected to Atlas");
+
+//     await initDB();
+
+//     mongoose.connection.close();
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+
+// const initDB = async () => {
+//     await Listing.deleteMany({});
+
+//     const listings = [];
+
+//     for (const obj of initData.data) {
+
+//         const response = await geocodingClient.forwardGeocode({
+//             query: `${obj.location}, ${obj.country}`,
+//             limit: 1,
+//         }).send();
+
+//         listings.push({
+//             ...obj,
+//             owner: "6a4a76dc83ab714cda5e4da2",
+//             geometry: response.body.features[0].geometry,
+//         });
+//     }
+
+//     await Listing.insertMany(listings);
+
+//     console.log("Database initialized successfully!");
+// };
+
+// main();
